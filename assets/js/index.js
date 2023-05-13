4e70290a7812770fe80879ce0dcf417b2dd8e3da
Juan Sepulveda <cjfsepu@protonmail.com>
2023-05-13 18:23:34 -0300

    add: details and contact

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,16 +2,41 @@ const cardsContainer = document.getElementById("cards-container")
 const checkboxContainer = document.getElementById("checkbox-container")
 let searchId = document.getElementById("searchId")
 let checkboxItems = Array.from(document.querySelectorAll(".check-item"))
+const detailsContainer = document.getElementById("details-container")
 
 async function getEvents() {
   let response = await fetch ("https://amd-amazingevents-api.onrender.com/api/eventos")
   let position = location.search.slice(10)
   let pathname = location.pathname
   let data = await response.json()
-    let currentDate = new Date(data.fechaActual.split('-').join('/'))
-  
+  let currentDate = new Date(data.fechaActual.split('-').join('/'))
 
-  // Si esta en el html de estadisticas, setea datos e inicia
+  if (pathname == "/pages/contact.html") {
+    let form = document.querySelector("form")
+    let modal = document.getElementById("modal")
+
+    function actionForm(e) {
+      e.preventDefault()
+      let formData = {
+        email: e.target[1].value,
+        name: e.target[0].value,
+        message: e.target[2].value
+      } 
+      form.classList.add("visibility-hidden", "h-25")
+      modalTemplate = `
+          <h3 class="mb-2 text-center">Gracias <b>${formData.name}</b></h4>
+          <h5 class="m-3 text-center">Tu mensaje: <i>${formData.message}</i></p>
+          <p class="m-3 text-center">ha sido enviado correctamente.</p>
+          <p class="mb-2 text-center">¡Pronto te redireccionaremos al inicio!</p>
+      `
+      modal.innerHTML += modalTemplate 
+      setTimeout(() => {window.location.pathname="/index.html"}, 10000)
+    }
+    form.addEventListener("submit", function(e){actionForm(e)})
+  }
+
+
+  // Si esta en el html de estadisticas, sete a datos e inicia
   if (pathname == "/pages/statistics.html") {
     eventsData = data.eventos
     // funcion principal
@@ -79,8 +104,6 @@ async function getEvents() {
       let pastEvents = []
       let futureEvents = []
       eventsData.filter(event => { event.assistance ? pastEvents.push(event) : futureEvents.push(event) })
-      // console.log(pastEvents)
-      // console.log(futureEvents)
 
       pastEvents.map(event => {event.percentageAssistance = event.assistance * 100 / event.capacity })
       
@@ -89,8 +112,6 @@ async function getEvents() {
       // mayor valor de asistencia 
       let maxAssistance = Math.max(...eventsAssistance)
       // Filtro en base al valor previo
-      // console.log(eventsData)
-      // console.log(eventsData.filter(event => {event === maxAssistance}))
       let majorAssistanceEvent = eventsData.filter(event => event.percentageAssistance == maxAssistance)
       // menor valor de asistencia
       let minAssistance = Math.min(...eventsAssistance)
@@ -148,9 +169,47 @@ async function getEvents() {
         }
       })    
     }
-
-
     initStatistics()
+  }
+
+  if (pathname == "/pages/details.html") {
+    let id = location.search.slice(1)
+
+    eventsData = data.eventos
+    let eventDetails = eventsData.filter(event => id == event.id)
+
+    // oculto fechas de navegacion, seteo nombre del evento
+    const leftArrow = document.getElementById("leftArrow")
+    const rightArrow = document.getElementById("rightArrow")
+    const menuTitle = document.getElementById("current")
+    leftArrow.style.visibility = "hidden"
+    menuTitle.textContent = eventDetails[0].name
+    rightArrow.style.visibility = "hidden"
+
+    // crear detalles de un evento
+    function createDetails(e) {
+      let detailsTemplate 
+      if (new Date(e.date) < currentDate){
+        detailsTemplate = `
+          <img src="${e.image}" class="details-img mt-4 mb-4 align-center">
+          <h5 class="mb-2">${e.description}</h4>
+          <p class="m-2 ">Este evento ocurrio en "<b>${e.place}</b>" el día <b>${e.date}</b></p>
+          <p class="mb-2">Lo sentimos. Ya no es posible comprar entrada para el evento.</p>
+          <p class="mb-3">¡A este evento asistieron un total de ${e.assistance} personas!</p>
+        `
+      }
+      else {
+        detailsTemplate = `
+          <img src="${e.image}" class="details-img mt-4 mb-4 align-center">
+          <h5 class="mb-2">${e.description}</h4>
+          <p class="m-2 ">Te esperamos en "<b>${e.place}</b>" el día <b>${e.date}.</b></p>
+          <p class="mb-3">A un precio especial de <b>$${e.price}</b></p>
+        `
+      }
+      detailsContainer.innerHTML += detailsTemplate
+    }
+    createDetails(eventDetails[0])
+
   }
 
   if (pathname == "/index.html" || pathname == "/") {
@@ -197,6 +256,7 @@ async function getEvents() {
     checkboxItems.forEach(checkbox => checkbox.addEventListener('click', filter))
     // eventlistener para el filtro de texto
     searchId.addEventListener('input', filter)
+    searchId.classList.remove("visibility-hidden")
     // filtro por checkbox
     function checkboxFilter(event) {
       let filter = checkboxItems.filter(checkbox => checkbox.checked).map(checkbox => checkbox.value)
@@ -206,11 +266,11 @@ async function getEvents() {
       }
       return event
     }
+
     // filtro de cartas
     function filterCards() {
       let checksFilter = checkboxFilter(eventsData)
       let filteredSearch = searchFilter(checksFilter, searchId.value)
-      console.log(filteredSearch)
       if(filteredSearch.length !== 0) {
         cardsContainer.innerHTML = ``
       }
@@ -222,9 +282,9 @@ async function getEvents() {
 // crear checkboxs
 function createCheckbox(e) {
   checkboxContainer.innerHTML += `
-  <label>
+  <label class="d-flex justify-between">
   <input class="checkbox check-item" value="${[e]}" type="checkbox">
-  <div class="checkmark"><span class="category">${[e]}</span></div>
+  <div class="checkmark mx-2"><span class="category">${[e]}</span></div>
   </label>
   `
 }
@@ -253,13 +313,14 @@ function createCard(e) {
   <p class="d-flex justify-content-center align-items-center card-description">${e.description}</p>
   <div class="d-flex row-2 justify-content-end">
   <p class="card-text col align-self-center">$${e.price}</p>
-  <a href="${e._id}" class="btn btn-primary h-100 w-50">Mas Detalles</a>
+  <a href="/pages/details.html?${e.id}" class="btn btn-primary h-100 w-50">Mas Detalles</a>
   </div>
   </div>
   </div>
   `
   cardsContainer.innerHTML += cardTemplate
 }
+
 // debounce para que la ejecucion del filtrado ocurra luego de terminar de tipear
 function debounce(func, timeout = 500){
   let timer;
